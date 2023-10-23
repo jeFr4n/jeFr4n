d443b29abd3eded035ecdc8e9eaccbc50668d557
devFran <124153964+jeFr4n@users.noreply.github.com>
2023-10-24 01:30:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
+ consoleText(['Hello World.', '</>', 'devfran'], 'text',['tomato','rebeccapurple','lightblue']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
